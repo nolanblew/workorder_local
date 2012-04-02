adedb78f1ac3b9f78d2ede5add9de41b320e94db
Nolan Blew <nolanblew@hotmail.com>
2012-04-02 16:44:40 -0700

    Initial commitit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkorderLocal::Application.config.secret_token = '274ab6f04af14a8598cc464847185e938485f02c1fb6d4573b8ebf3d8f4f537c39ce072b0d53426c6ddea8f319c061dd0c252eae105a8affc18c0bb607ec33e4'
